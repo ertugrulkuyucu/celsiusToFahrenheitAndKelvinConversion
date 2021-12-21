54f90a6c9f6c0317023f9b7818d804dea68fa3c6
Ertuğrul Kuyucu <75902272+ertugrulkuyucu@users.noreply.github.com>
2021-12-21 19:34:49 +0300

    1

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace celsiusDonusum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+
+            double kelvin, fahrenheit;
+
+
+            double celsius = 0;
+            celsius = Convert.ToSingle(celsiusText.Text);
+
+            kelvin = celsius + 273.15d;
+            fahrenheit = (celsius * 9d / 5d) + 32d;
+
+            fahrenheitTxt.Text = fahrenheit.ToString();
+            kelvinTxt.Text = kelvin.ToString();
+              
+            // deneme
+
+        }
+
+        private void fahrenheitTxt_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
